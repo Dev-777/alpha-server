c73b0beab80c0c884c7fb977f204f9f7cdcab087
Dev-777 <arsen172317@gmail.com>
2021-07-09 22:32:53 +0400

    mongoose is in progress, eod

---
+++ models/user/User.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const userModel = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = mongoose.model("user", userModel);
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require("express");
-const bodyParser=require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
-const routes = require('./routes')
-const port = process.env.PORT || 3000;
+const routes = require("./routes");
+const port = process.env.PORT || 4000;
+const cors = require("cors");
+const mongoose = require("mongoose");
 
+app.use(require("morgan")("dev"));
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
-app.use('/',routes)
+mongoose.connect(
+  process.env.DATABASE_ACCES,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("database connected!!!")
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/", routes);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
